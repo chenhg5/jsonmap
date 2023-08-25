c7a756839211e07380982f20020bb9b9b8a2e769
cg33 <chg80333@163.com>
2023-08-25 00:53:40 +0000

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # jsonmap
 golang json serialization field mapper, using struct tag
+
+## Usage
+
+```go
+package main
+
+import (
+    "fmt"
+
+    "github.com/chenhg5/jsonmap"
+)
+
+func main() {
+
+    var text = `{
+		"type": "cow"
+	}`
+
+	type Animal struct {
+		Type uint8 `json:"type" jsonmap:"0:dog;1:cat;2:cow;3:others"`
+	}
+
+	var animal = Animal{}
+	json.Unmarshal([]byte(text), Wrap(&animal))
+
+    // Animal{Type: 2}
+
+    res, _ := json.MarshalIndent(Wrap(animal), "", "    ")
+    fmt.Println(res)
+
+    // {"type": "cow"}
+}
+
+
+```
--- jsonmap_test.go
+++ jsonmap_test.go
@@ -28,3 +28,33 @@ func TestJSONMapUnmarshal(t *testing.T) {
 	err := json.Unmarshal([]byte(text), Wrap(&animal))
 	t.Logf("\nres: %+v\nerr %+v", animal, err)
 }
+
+func BenchmarkJSONMapUnmarshal(b *testing.B) {
+	var text = `{
+		"type": "cow"
+	}`
+
+	type Animal struct {
+		Type uint8 `json:"type" jsonmap:"0:dog;1:cat;2:cow;3:others"`
+	}
+
+	for i := 0; i < b.N; i++ {
+		var animal = Animal{}
+		json.Unmarshal([]byte(text), Wrap(&animal))
+	}
+}
+
+func BenchmarkJSONUnmarshal(b *testing.B) {
+	var text = `{
+		"type": "cow"
+	}`
+
+	type Animal struct {
+		Type uint8 `json:"type" jsonmap:"0:dog;1:cat;2:cow;3:others"`
+	}
+
+	for i := 0; i < b.N; i++ {
+		var animal = Animal{}
+		json.Unmarshal([]byte(text), &animal)
+	}
+}
